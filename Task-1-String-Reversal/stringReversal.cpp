6a58d31935692652b928b2ef210352aca232046b
Ankit Singh <ankitsingh19082003@gmail.com>
2025-12-16 02:33:00 +0530

    Finalize Task 1 clean structure

--- Task-1-String-Reversal/stringReversal.cpp
+++ Task-1-String-Reversal/stringReversal.cpp
@@ -1,25 +1,31 @@
+/*
+Name: Ankit Singh
+Internship: C/C++ Programming Intern – Cognifyz Technologies
+Task: Task 1 – String Reversal
+Description: Reverses a given string using two-pointer technique.
+*/
+
 #include <iostream>
 #include <string>
-#include <utility>
 using namespace std;
 
 void stringReversal(string &str) {
-  int i = 0;
-  int j = str.length() - 1;
+    int i = 0;
+    int j = str.length() - 1;
 
-  while (i <= j) {
-
-    swap(str[i], str[j]);
-    i++;
-    j--;
-  }
-  return;
+    while (i < j) {
+        swap(str[i], str[j]);
+        i++;
+        j--;
+    }
 }
 
 int main() {
-  string str = "helloWorld";
-  cout << "String before reversal : " << str << endl;
+    string str = "helloWorlld";
 
-  stringReversal(str);
-  cout << "String after reversal : " << str << endl;
+    cout << "String before reversal: " << str << endl;
+    stringReversal(str);
+    cout << "String after reversal: " << str << endl;
+
+    return 0;
 }
